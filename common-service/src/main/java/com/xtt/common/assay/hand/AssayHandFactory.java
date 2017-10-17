939adb904dddf1c7fddbc944fc58b28568b46aac
xw <xw@xtt.com>
2017-10-17 11:19:38 +0800

    修改化验清洗，返回结果为查询到的化验日期

--- common-service/src/main/java/com/xtt/common/assay/hand/AssayHandFactory.java
+++ common-service/src/main/java/com/xtt/common/assay/hand/AssayHandFactory.java
@@ -125,7 +125,7 @@ public abstract class AssayHandFactory {
         AssayFilterRule assayFilterRule = assayFilterRuleService.getByTenantId(UserUtil.getTenantId());
         Map<String, PatientAssayRecordPO> assayHospDictMap = new HashMap<>();
         Set<String> existsInspectionId = new HashSet<>(listPatientAssayRecord.size());
-        // 转换过日期数据
+        // 数据来源表查询到的有效的数据日期
         Set<String> dateSet = new TreeSet<>();
         for (PatientAssayRecordPO patientAssayRecord : listPatientAssayRecord) {
             // 检查项目唯一ID、itemCode、组id、化验结果等任意一项数据为空则不处理
@@ -142,6 +142,8 @@ public abstract class AssayHandFactory {
             if (patientAssayRecord.getReportTime() == null) {// 如果报告时间为空，设置报告时间的值为样品时间的
                 patientAssayRecord.setReportTime(patientAssayRecord.getSampleTime());
             }
+            Date assayDate = patientAssayRecord.getSampleTime();// 检查日期默认取样品时间
+            dateSet.add(DateFormatUtil.convertDateToStr(assayDate));// 添加查询到的数据日期
             int countByInspectionId = patientAssayRecordBusiService.countByInspectionId(patientAssayRecord.getInspectionId(),
                             patientAssayRecord.getFkTenantId());
             if (countByInspectionId != 0) {
@@ -162,7 +164,7 @@ public abstract class AssayHandFactory {
             patientAssayRecordBusi.setUpdateTime(nowDate);
             patientAssayRecordBusi.setCreateUserId(CommonConstants.SYSTEM_USER_ID);
             patientAssayRecordBusi.setUpdateUserId(CommonConstants.SYSTEM_USER_ID);
-            patientAssayRecordBusi.setAssayDate(patientAssayRecord.getSampleTime());// 检查日期默认取样品时间
+            patientAssayRecordBusi.setAssayDate(assayDate);
             patientAssayRecordBusi
                             .setAssayMonth(DateFormatUtil.convertDateToStr(patientAssayRecordBusi.getAssayDate(), DateFormatUtil.FORMAT_YYYY_MM));
             patientAssayRecordBusi.setResultActual(matcherToNum(patientAssayRecordBusi.getResult()));
@@ -175,7 +177,6 @@ public abstract class AssayHandFactory {
             if (inspectioidBack != null) {
                 inspectioidBackList.add(inspectioidBack);
             }
-            dateSet.add(DateFormatUtil.convertDateToStr(patientAssayRecordBusi.getAssayDate()));
             listPatientAssayRecordBusi.add(patientAssayRecordBusi);
             i++;
             if (i == 1000) {
@@ -229,7 +230,7 @@ public abstract class AssayHandFactory {
      * @param startCreateTime
      * @param endCreateTime
      * @param mapPatientId
-     *
+     * @return 所有查询到数据的化验日期
      */
     public Set<String> save(Date startCreateTime, Date endCreateTime, Long fkPatientId, Map<Long, List<Date>> mapPatientId) {
         List<PatientAssayRecordPO> listAssayRecord = listPatientAssayRecordByCreateTime(startCreateTime, endCreateTime, fkPatientId);
